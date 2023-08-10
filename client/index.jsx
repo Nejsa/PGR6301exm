891b5a52ac808fbd185495c92c91598b5639a190
noman <noman99_@hotmail.com>
2023-08-10 05:45:21 +0200

    Added two other react pages: logHours and Activities, the links are working and sending the user to another page.

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,52 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+
+function FrontPage() {
+    return(
+        <div>
+            <h1> Home page for Activities</h1>
+            <ul>
+                <li>
+                    <Link to={"/activities/logHours"}> Log your hours </Link>
+                </li>
+                <li>
+                    <Link to={"/activities/showActivities"}> Show Available Activities </Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+function Activities(){
+    return(
+        <Routes>
+            <Route path={"/logHours"} element={<h1> Log your Hours </h1>} />
+            <Route path={"/showActivities"} element={<h1> Show Activities Available </h1>} />
+        </Routes>
+    );
+}
+
+
+function Application() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<FrontPage />} />
+                <Route path={"/activities/*"} element={<Activities />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+// root.render(<h1>Something Displayed!</h1>)
 
 const element = document.getElementById("app");
 const root = createRoot(element);
-root.render(<h1>Something Displayed!</h1>)
+
+root.render(<Application />);
+
+
+
+
+
